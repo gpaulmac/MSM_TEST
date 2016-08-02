401c02de709198fa93dfb5f0dbcf7c8805aff50c
gpaulmac <gpaulmac@Pauls-MacBook-Pro.local>
2016-08-02 13:17:42 +0100

    v1
    Initial Commit

---
+++ src/com/sample/util/PeelerUtil.java
@@ -0,0 +1,14 @@
+package com.sample.util;
+
+import com.sample.bean.Apple;
+
+public class PeelerUtil {
+	
+	public static boolean canPeel(Apple a){
+		boolean canPeel=false;
+		if(!a.hasWorm() && a.getTaste() >3){
+			canPeel=true;
+		}
+		return canPeel;
+	}
+}
